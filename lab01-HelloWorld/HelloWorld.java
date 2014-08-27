d6098dfe0c3bcafb00d388f7fc6cd93ca89aea80
rreplogle <rar218@lehigh.edu>
2014-08-27 20:51:22 +0000

    Lab 01 update

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+//Hello World Program - Lab 01 - CSE 02
+
+//creates a class
+public class HelloWorld{
+     
+     
+    //creates the main program (method)
+    public static void main(String[] args){
+            
+        //prints "Hello World" with a box around it
+        System.out.println("##################");
+        System.out.println("##################");
+        System.out.println("##################");
+        System.out.println("#- Hello  World -#");
+        System.out.println("##################");
+        System.out.println("##################");
+        System.out.println("##################");
+            
+    }
+}
