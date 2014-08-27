d00caa0b724ebcce25a1c4548554faea00a29b66
rreplogle <rar218@lehigh.edu>
2014-08-27 21:14:04 +0000

    Welcome CLass update

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Welcome Class Program - HW 01 - CSE 02
+
+//creates a class
+public class WelcomeClass{
+     
+     
+    //creates the main program (method)
+    public static void main(String[] args){
+            
+        //prints the welcome message and statment
+        System.out.println("-------------");
+        System.out.println("---WELCOME---");
+        System.out.println("-------------");
+        System.out.println("  ____________ ");
+        System.out.println(" /           /");
+        System.out.println("/R A R 2 1 8/");
+        System.out.println("============");
+        System.out.println("I have backpacked over 700 miles ");
+        System.out.println("in over 5 different countries.");
+        System.out.println("I am also an Eagle Scout.");    
+    }
+}
